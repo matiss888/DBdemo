d5c46ebbf529814ff4d9663cb8afeda34551dbdb
Matiss <matissciss@gmail.com>
2026-07-04 11:05:54 +0300

    create.sql TODOs completed

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -4,19 +4,17 @@ PRAGMA foreign_keys = ON;
 DROP TABLE IF EXISTS students;
 DROP TABLE IF EXISTS courses;
 
--- TODO 1:
--- Create the courses table.
--- It must contain:
--- course_id   INTEGER primary key
--- course_name TEXT, required
--- credits     INTEGER, must be greater than 0
+CREATE TABLE courses (
+    course_id   INTEGER PRIMARY KEY,
+    course_name varchar(255) NOT NULL,
+    credits     INTEGER CHECK ( credits > 0 )
+);
 
--- TODO 2:
--- Create the students table.
--- It must contain:
--- id        INTEGER primary key
--- name      TEXT, required
--- email     TEXT, required and unique
--- age       INTEGER, must be 18 or older
--- course_id INTEGER, required
--- course_id must be a foreign key that references courses(course_id).
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    age INTEGER CHECK ( age >= 18 ),
+    course_id INTEGER NOT NULL,
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
